831094577bb07f6a82acb2932a432cda4db1a20c
Jake Zegil <jakezegil@gmail.com>
2024-07-11 20:52:07 -0400

    fix merge issue

--- commands/auth.ts
+++ commands/auth.ts
@@ -16,7 +16,6 @@ const authCommand = (cli: Argv) =>
           description: "The action to perform: signup, login, logout, update",
           choices: ["signup", "login", "logout", "update"],
           type: "string",
-          choices: ["signup", "login", "logout"],
           demandOption: true,
         })
         .option("key", {
