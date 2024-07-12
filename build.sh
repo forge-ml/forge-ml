93f423793dfb30059a1e092a2d82766bbd235963
Jake Zegil <jakezegil@gmail.com>
2024-07-12 14:24:53 -0400

    version change

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
-npm run build
-
-## add the shebang to build output
-echo '#!/usr/bin/env node' | cat - bin/index.js > temp && mv temp bin/index.js
+bun build --compile --target=bun-linux ./index.ts --outfile ./bin/linux # Current arch/os
+bun build --compile --target=bun-linux-arm64 ./index.ts --outfile ./bin/linux-arm64 # Specific arch/os
+bun build --compile --target=bun-linux-x64-v1.1.4 ./index.ts --outfile ./bin/linux-x64-v1.1.4
+bun build --compile --target=bun-linux-arm64-v1.1.4 ./index.ts --outfile ./bin/linux-arm64-v1.1.4
+bun build --compile --target=bun-mac-arm64-v1.1.4 ./index.ts --outfile ./bin/mac-arm64-v1.1.4
+bun build --compile --target=bun-mac-x64-v1.1.4 ./index.ts --outfile ./bin/mac-x64-v1.1.4
+bun build --compile --target=bun-arm64-mac-v1.1.4 ./index.ts --outfile ./bin/arm64-mac-v1.1.4
