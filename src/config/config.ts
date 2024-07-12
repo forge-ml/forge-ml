5206fa03247a5eb9af4f3c9a64d24b48d7dd8571
Mehul Behera <mbehera@umd.edu>
2024-07-12 17:39:22 -0400

    fixed serverURL config

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,7 +5,7 @@ const bin = "forge";
 const LOCAL_SERVER = process.env.LOCAL_SERVER;
 
 export const config = {
-  serverUrl: "http://localhost:3000",
+  serverUrl: LOCAL_SERVER || "https://api.forge-ml.com",
   schemaPath: "forge",
   apiKeyFilePath: os.homedir() + `/.${bin}/key.json`,
   bin,
