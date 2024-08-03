11d091e06bcad7bed236bb968f00e8d19ea03cc5
Mehul Behera <mbehera@umd.edu>
2024-08-03 14:06:49 -0400

    update docs functionality

--- src/commands/docs.ts
+++ src/commands/docs.ts
@@ -2,6 +2,23 @@ import type { Argv } from "yargs";
 import authGate from "../utils/authGate";
 import cWrap from "../utils/logging";
 import makeRequest, { EP } from "../utils/request";
+import { exec } from "node:child_process";
+import localConfigService from "../controls/auth/svc";
+
+function getForgeKey() {
+  const provider = "forge";
+  const key = localConfigService.getValue(provider);
+  if (!key) {
+    console.log(`${cWrap.br("Error")} copying key.`);
+    return;
+  }
+  // ONLY WORKS ON MACOS
+  const proc = require("child_process").spawn("pbcopy");
+  proc.stdin.write(`Bearer ${key}`);
+  proc.stdin.end();
+
+  console.log(`Key copied to clipboard for ${cWrap.fg(provider)}.`);
+}
 
 const docsCommand = (cli: Argv) =>
   cli.command(
@@ -17,9 +34,27 @@ const docsCommand = (cli: Argv) =>
         console.log(`${cWrap.br("Error")} fetching docs url.`);
         return;
       } else {
-        console.log(
-          `Your docs are available at: ${cWrap.fg(response.data.url)}`
-        );
+        const url = response.data.url;
+        console.log(`Your docs are available at: ${cWrap.fg(url)}`);
+
+        //copy forge key to clipboard
+        getForgeKey();
+
+        // Open the URL in the default browser
+        const command =
+          process.platform === "win32"
+            ? "start"
+            : process.platform === "darwin"
+            ? "open"
+            : "xdg-open";
+        exec(`${command} ${url}`, (err) => {
+          // SHOUlD HAVE AN YES OR NO OPTION TO OPEN THE URL IN THE BROWSER
+          if (err) {
+            console.error("Failed to open the docs in the browser:", err);
+          } else {
+            console.log("Your docs have been opened in your default browser.");
+          }
+        });
       }
     }
   );
